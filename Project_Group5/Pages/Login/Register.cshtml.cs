c0f5559849c17b4ee1f199378799015d38a98a2f
truongthinh <lecongtruongthinh@gmail.com>
2024-10-24 15:58:54 +0700

    HOmepage, login,resgiter

---
+++ Project_Group5/Pages/Login/Register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Project_Group5.Pages.Login
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
