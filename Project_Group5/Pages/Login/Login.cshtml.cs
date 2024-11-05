2e360bb4954656936eead02b4695bf335633f21c
hoangnxhe171096 <hoangnxhe171096@fpt.edu.vn>
2024-11-05 13:56:32 +0700

    Revert "Merge branch 'Hoang_Admin' of https://github.com/thinhneff1509/PRN221_HotelManager"
    This reverts commit 2ce54103acca740144f58e57a4031b00be1d24ec, reversing
    changes made to 377ff1fd9da727e263e75452e77fee2d3226bd3c.

--- Project_Group5/Pages/Login/Login.cshtml.cs
+++ Project_Group5/Pages/Login/Login.cshtml.cs
@@ -41,8 +41,8 @@ namespace Project_Group5.Pages.Login
 			{
 				new Claim(ClaimTypes.Name, customer.Username),
 				new Claim(ClaimTypes.NameIdentifier, customer.Id.ToString()),
-				new Claim(ClaimTypes.Role, customer.Role.Name)
-			};
+				new Claim(ClaimTypes.Role, customer.Role.Name) 
+            };
 
 			var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
 
@@ -53,7 +53,6 @@ namespace Project_Group5.Pages.Login
 			return RedirectToPage("/Homepage/Home");
 		}
 
-
 		public async Task<IActionResult> OnGetLogoutAsync()
 		{
 			await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
@@ -61,4 +60,3 @@ namespace Project_Group5.Pages.Login
 		}
 	}
 }
-
