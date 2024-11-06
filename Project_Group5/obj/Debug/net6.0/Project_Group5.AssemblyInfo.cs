c972bdf6a39ee6c223018f1d75dd784b311c9020
HollowLights <thinhnthe171628@fpt.edu.vn>
2024-11-06 20:55:08 +0700

    Revert "Fix conflict"
    This reverts commit 35a9c12c9a4087b166c03422a8b1593c49edac95.

--- Project_Group5/obj/Debug/net6.0/Project_Group5.AssemblyInfo.cs
+++ Project_Group5/obj/Debug/net6.0/Project_Group5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project_Group5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c0198a80572173a533cc476b0f3a3029f453e36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4594e813723ed4919a43cf81421df9e79976c70")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project_Group5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project_Group5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
