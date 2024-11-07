e7c9fd5fb0dd5fdd7fbcaa67b19d46bac9dea489
truongthinh <lecongtruongthinh@gmail.com>
2024-11-07 12:59:33 +0700

    merge hoang paymentadmin vs quang feedback admin

--- Project_Group5/obj/Debug/net6.0/Project_Group5.AssemblyInfo.cs
+++ Project_Group5/obj/Debug/net6.0/Project_Group5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project_Group5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dcb4005d79ccd68a958a33757f1d7f66a646f73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2910146a90788b6439cd0ff9fc5449b39732fdf4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project_Group5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project_Group5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
