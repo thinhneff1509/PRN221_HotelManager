4105dc4f1a859936797a93f554271a42f83fce18
truongthinh <lecongtruongthinh@gmail.com>
2024-11-07 10:39:43 +0700

    done

--- Project_Group5/obj/Debug/net6.0/Project_Group5.AssemblyInfo.cs
+++ Project_Group5/obj/Debug/net6.0/Project_Group5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project_Group5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+904edcf61709bb29d52f82a4229c8e78a4f93ce8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dcb4005d79ccd68a958a33757f1d7f66a646f73")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project_Group5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project_Group5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
