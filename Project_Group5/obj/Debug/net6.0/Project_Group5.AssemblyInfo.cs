05217215240646002832f5c9704564536b47a5bb
HollowLights <thinhnthe171628@fpt.edu.vn>
2024-11-07 13:23:17 +0700

    Pull Thinh_Autehn

--- Project_Group5/obj/Debug/net6.0/Project_Group5.AssemblyInfo.cs
+++ Project_Group5/obj/Debug/net6.0/Project_Group5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project_Group5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b837b39c7bbfeb872fed09ea1b9a85bb3f58435")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e13e76378dfc87421ae5e00d81a2c93ecf9ca1e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project_Group5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project_Group5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
