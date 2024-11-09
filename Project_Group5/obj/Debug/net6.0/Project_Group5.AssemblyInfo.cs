b628fd57d8251381a5b9f1655cc4db7d85c610f3
truongthinh <lecongtruongthinh@gmail.com>
2024-11-09 23:37:27 +0700

    delete gg

--- Project_Group5/obj/Debug/net6.0/Project_Group5.AssemblyInfo.cs
+++ Project_Group5/obj/Debug/net6.0/Project_Group5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project_Group5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3389ec1a055197b318e312757984cf6f895f5a37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+794128f883adb94fd3a0aa7e3d6c5955c72543be")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project_Group5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project_Group5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
