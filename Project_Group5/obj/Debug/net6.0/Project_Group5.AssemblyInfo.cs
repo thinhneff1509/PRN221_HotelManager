e75d067bf0243ccb68cdd223b42de7a44d496813
truongthinh <lecongtruongthinh@gmail.com>
2024-11-11 14:13:27 +0700

    mergecode

--- Project_Group5/obj/Debug/net6.0/Project_Group5.AssemblyInfo.cs
+++ Project_Group5/obj/Debug/net6.0/Project_Group5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project_Group5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f58593c8e155dc4602f8d9a0cda56a470b9f11e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c6140090ca3e2497dc438943d5e369f2a510b07")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project_Group5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project_Group5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
